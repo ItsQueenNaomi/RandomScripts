9d7f6b677843224406a9550eaabaf84404a6b303
ItsQueenNaomi <59656050+ItsQueenNaomi@users.noreply.github.com>
2024-12-12 10:06:10 -0600

    added license information to the top of each script

--- File Mode Scripts/rp.cpp
+++ File Mode Scripts/rp.cpp
@@ -1,3 +1,20 @@
+/*
+  File read permission granter (or denier). Will add (or remove) file permission mode 'r' for all specified files for the respective categories.
+  Copyright (C) 2024  Aristotle Daskaleas
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 // This program is designed to recursively add read permissions for specified files
 
 #include <iostream>
